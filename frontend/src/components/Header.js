3512f18b1517620224aaba56acc99b77b3468536
ripon272717 <mojam688388@gmail.com>
2026-01-31 16:44:51 +0300

    Favicon change

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -52,6 +52,7 @@ const Header = () => {
       navigate("/search")
     }
   }
+  
   return (
     <header className='h-16 shadow-md bg-white fixed w-full z-40'>
       <div className=' h-full container mx-auto flex items-center px-4 justify-between'>
